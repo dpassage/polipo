5420952eeda543eb3d03a7f46112809eb2658132
Alexsander Akers <a2@pandamonia.us>
2012-11-12 19:31:55 -0500

    Fix implementation of memrchr
    Signed-off-by: Alexsander Akers <a2@pandamonia.us>
    
      + Non-author changes to origial patch:
      * Trailing whitespace deleted.

--- util.c
+++ util.c
@@ -199,12 +199,16 @@ isWhitespace(const char *string)
 void *
 memrchr(const void *s, int c, size_t n)
 {
-    const unsigned char *ss = s;
-    unsigned char cc = c;
-    size_t i;
-    for(i = 1; i <= n; i++)
-        if(ss[n - i] == cc)
-            return (void*)(ss + n - i);
+    if (n != 0)
+    {
+        const unsigned char *cp = (unsigned char *) s + n;
+        do
+        {
+            if (*(--cp) == (unsigned char) c)
+                return (void *) cp;
+        } while (--n != 0);
+    }
+
     return NULL;
 }
 #endif
